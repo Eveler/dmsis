ff452d47be16fed49a7a1d8c08e1b0a853d95876
Mike Savenko <eveler555@gmail.com>
2017-10-05 23:01:34 +1000

    trying ladon

--- main.py
+++ main.py
@@ -2,12 +2,45 @@
 
 # Author: Savenko Mike
 
-import pyximport
-pyximport.install()
-import hello
-pyximport.uninstall(__import__, pyximport)
-import hellop
+from ladon.compat import PORTABLE_STRING
+from ladon.ladonizer import ladonize
+from ladon.types.ladontype import LadonType
 
-if __name__ == '__main__':
-    hello.say_hello("Cython")
-    hellop.say_hello("Python")
+# import pyximport
+# pyximport.install()
+# import hello
+# pyximport.uninstall(__import__, pyximport)
+# import hellop
+
+
+# class Declar(LadonType):
+#     number = PORTABLE_STRING
+#
+#
+# class SoapService:
+#     @ladonize(Declar, rtype=PORTABLE_STRING)
+#     def SendDeclar(self, declar):
+#         pass
+
+
+class Calculator(object):
+    """
+    This service does the math, and serves as example for new potential Ladon
+    users.
+    """
+
+    @ladonize(int, int, rtype=int)
+    def add(self, a, b):
+        """
+        Add two integers together and return the result
+    
+        @param a: 1st integer
+        @param b: 2nd integer
+        @rtype: The result of the addition
+        """
+        return a + b
+
+
+# if __name__ == '__main__':
+#     # hello.say_hello("Cython")
+#     hellop.say_hello("Python")
