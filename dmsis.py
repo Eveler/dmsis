dcaa9b29aedb42df19d1285b486689c2b2047e86
Eveler <evelerwork@mail.ru>
2017-11-28 19:32:17 +1000

    Manager in progress

--- dmsis.py
+++ dmsis.py
@@ -6,8 +6,6 @@
 # Then it queries Directum for changed status for stored requests and sends
 # SendResponse if status changed
 import logging
-from sys import exc_info
-from traceback import format_exception
 
 from smev import Adapter
 
@@ -16,6 +14,8 @@ class Integration:
     def __init__(self, use_config=True, config_path='./dmsis.ini'):
         if use_config:
             self.parse_config(config_path)
+        else:
+            self.mail_addr = ''
         self.smev = Adapter()
 
     def parse_config(self, config_path):
@@ -71,6 +71,16 @@ class Integration:
             logging.info("Set logging level to '%s'", loglevel)
             logging.root.setLevel(loglevel)
 
+            if 'mail_addr' not in cfg.options('main'):
+                do_write = True
+                cfg.set('main', 'mail_addr', 'ioib@adm-ussuriisk.ru')
+            self.mail_addr = cfg.get('main', 'mail_addr')
+
+            if 'mail_server' not in cfg.options('main'):
+                do_write = True
+                cfg.set('main', 'mail_server', '192.168.1.6')
+            self.mail_server = cfg.get('main', 'mail_server')
+
             if do_write:
                 for fn in lst:
                     with open(fn, "w") as configfile:
@@ -85,8 +95,24 @@ class Integration:
             self.report_error(e)
 
     def report_error(self, e):
+        from sys import exc_info
+        from traceback import format_exception
         logging.error(e.message)
         etype, value, tb = exc_info()
         trace = ''.join(format_exception(etype, value, tb))
-        msg = "\n" + "*" * 70 + "\n%s: %s\n%s\n" + "*" * 70
-        logging.error(msg % (etype, value, trace))
+        msg = "\n" + "*" * 70 + "\n%s: %s\n%s\n" + "*" * 70  % \
+                                                   (etype, value, trace)
+        logging.error(msg)
+
+        if self.mail_server:
+            import smtplib
+            from email.mime.text import MIMEText
+            from_addr = 'admin@adm-ussuriisk.ru'
+            message = MIMEText(msg)
+            message['Subject'] = 'SMEV service error'
+            message['From'] = from_addr
+            message['To'] = self.mail_addr
+
+            s = smtplib.SMTP(self.mail_server)
+            s.sendmail(from_addr, [self.mail_addr], message.as_string())
+            s.quit()
