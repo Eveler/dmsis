af5d13e762ffbced660dd73dc03c237f4c84438a
Eveler <evelerwork@mail.ru>
2023-08-10 17:42:27 +1000

    class DirectumRX

--- plugins/dirrx.py
+++ plugins/dirrx.py
@@ -6,6 +6,13 @@ import logging
 from odata import ODataService
 from requests.auth import HTTPBasicAuth
 
+
+class DirectumRX:
+    def __init__(self, url, username='', password=''):
+        my_auth = HTTPBasicAuth(username, password) if username else None
+        self._service = ODataService(url, auth=my_auth, reflect_entities=True)
+
+
 if __name__ == '__main__':
     logging.basicConfig(
         level=logging.DEBUG,
@@ -20,11 +27,9 @@ if __name__ == '__main__':
     # http://192.168.0.134/Integration/odata/IMunicipalServicesServiceCases?$expand=*&$count=true&$filter=ServEndDateFact%20eq%20null
 
     url = "http://rxtest.adm-ussuriisk.ru/Integration/odata/"
-    # url = 'http://services.odata.org/V4/Northwind/Northwind.svc/'
-    my_auth = HTTPBasicAuth('Savenko', 'mY3pss2augo')
-    service = ODataService(url, auth=my_auth, reflect_entities=True)
-    declar = service.entities['IMunicipalServicesServiceCases']
-    query = service.query(declar)
+    rx = DirectumRX(url, 'Savenko', 'mY3pss2augo')
+    declar = rx._service.entities['IMunicipalServicesServiceCases']
+    query = rx._service.query(declar)
     # print(query.raw({'$expand': '*'}))
     query.options['$expand'] = ['Author', 'DeliveryMethod']
     # query.expand(declar.Author, declar.DeliveryMethod)
