4604156bf373bec7af0fbb9a17fcdd58919cdb31
Eveler <evelerwork@mail.ru>
2023-08-11 18:53:46 +1000

    class DirectumRX skeleton

--- plugins/dirrx.py
+++ plugins/dirrx.py
@@ -7,10 +7,50 @@ from odata import ODataService
 from requests.auth import HTTPBasicAuth
 
 
+class DirectumRXException(Exception):
+    def __init__(self, msg, *args, **kwargs):
+        super().__init__(msg, args, kwargs)
+
+    @property
+    def message(self):
+        return self.args[0]
+
+
 class DirectumRX:
+    REF = None
+
     def __init__(self, url, username='', password=''):
         my_auth = HTTPBasicAuth(username, password) if username else None
         self._service = ODataService(url, auth=my_auth, reflect_entities=True)
+
+    def add_declar(self, declar, files):
+        raise "Not released yet"
+
+    def run_script(self, script_name, params=()):
+        raise "Not released yet"
+
+    def search(self, code, criteria, tp=REF, order_by='', ascending=True, raw=False):
+        entity = self._service.entities[code]
+        query = self._service.query(entity)
+        query_params = {'$expand': '*'}
+        if criteria:
+            query_params['$filter'] = criteria
+        if order_by:
+            query_params['$orderby'] = order_by + (' asc' if ascending else ' desc')
+        return query.raw(query_params)
+
+    def get_result_docs(self, directum_id, crt_name='Администрация Уссурийского городского округа',
+                    zip_signed_doc=False, days_old=-3):
+        raise "Not released yet"
+
+    def get_declar_status_data(self, declar_id=None, fsuids: list = (), permanent_status='6'):
+        raise "Not released yet"
+
+    def update_reference(self, ref_name, rec_id, data):
+        raise "Not released yet"
+
+    def update_elk_status(self, data):
+        raise "Not released yet"
 
 
 if __name__ == '__main__':
@@ -27,14 +67,14 @@ if __name__ == '__main__':
     # http://192.168.0.134/Integration/odata/IMunicipalServicesServiceCases?$expand=*&$count=true&$filter=ServEndDateFact%20eq%20null
 
     url = "http://rxtest.adm-ussuriisk.ru/Integration/odata/"
-    rx = DirectumRX(url, 'Savenko', 'mY3pss2augo')
-    declar = rx._service.entities['IMunicipalServicesServiceCases']
-    query = rx._service.query(declar)
+    rx = DirectumRX(url, 'Service User', '[1AnAr1]')
+    res = rx.search('IMunicipalServicesServiceCases', 'ServEndDateFact eq null', order_by='Created', ascending=False)
+    for r in res:
+        print(r)
+    exit()
     # print(query.raw({'$expand': '*'}))
     query.options['$expand'] = ['Author', 'DeliveryMethod']
     # query.expand(declar.Author, declar.DeliveryMethod)
-    # query.filter(declar.ServEndDateFact == None)
-    query.options['$filter'] = ['ServEndDateFact eq null']
     for ent in query.all():
         print(ent.Name, ent.__dict__)
         print(ent.DeliveryMethod.Name, ent.Author.Name)
